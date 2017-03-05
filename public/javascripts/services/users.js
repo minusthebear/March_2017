57e5b266d1017d1b6c3d05a0a0f0b88840002ca7
matthewhamann <matt.hamann1982@gmail.com>
2017-03-05 15:52:37 -0800

    Updated users files, 3:52 pm

--- public/javascripts/services/users.js
+++ public/javascripts/services/users.js
@@ -1,10 +1,37 @@
 (function(){
 	angular.module("testing_app")
-		.factory("Users", function(){
+		.factory("UsersService", function(){
 			var Users = {};
 
-			Users.all = function(){
+			var userList = [
+				{
+					id: 1,
+					name: "Richard Hendricks",
+					email: "richard@piedpiper.com",
+					phone: 4085550011
+				},
+				{
+					id: 2,
+					name: "Erlich Bachman",
+					email: "erlich@aviato.com",
+					phone: 4155552233
+				},
+				{
+					id: 3,
+					name: "Gavin Belson",
+					email: "gavin@hooli.com",
+					phone: 9165554455
+				}
+			];
 
+			Users.all = function(){
+				return userList;
+			};
+
+			Users.findById = function(id){
+				return userList.find(function(user){
+					return user.id === id;
+				});
 			};
 
 			return Users;
--- public/javascripts/services/users.spec.js
+++ public/javascripts/services/users.spec.js
@@ -1,19 +1,62 @@
 describe("Users Factory", function(){
-	var Users;
+	var UsersService;
+
+	var userList = [
+		{
+			id: 1,
+			name: "Richard Hendricks",
+			email: "richard@piedpiper.com",
+			phone: 4085550011
+		},
+		{
+			id: 2,
+			name: "Erlich Bachman",
+			email: "erlich@aviato.com",
+			phone: 4155552233
+		},
+		{
+			id: 3,
+			name: "Gavin Belson",
+			email: "gavin@hooli.com",
+			phone: 9165554455
+		}
+	];
+
+	var singleUser = {
+		id: 2,
+		name: "Erlich Bachman",
+		email: "erlich@aviato.com",
+		phone: 4155552233
+	};
 
 	beforeEach(angular.mock.module("testing_app"));
 
-	beforeEach(inject(function(_Users_){
-		Users = _Users_;
+	beforeEach(inject(function(_UsersService_){
+		UsersService = _UsersService_;
 	}));
 
 	it("should exist", function(){
-		expect(Users).toBeDefined();
+		expect(UsersService).toBeDefined();
 	});
 
 	describe("Users.all()", function(){
 		it("should exist", function(){
-			expect(Users.all).toBeDefined();
+			expect(UsersService.all).toBeDefined();
+		});
+
+		it("should return the userlist", function(){
+			console.log(userList);
+			expect(UsersService.all()).toEqual(userList);
+		});
+	});
+
+	describe("Users.findById()", function(){
+		it("should exist", function(){
+			expect(UsersService.findById).toBeDefined();
+		});
+
+		it("should retrieve a user with the id of 2", function(){
+			expect(UsersService.findById(2)).toEqual(singleUser);
 		});
 	});
 });
